766ee8d5e0758dbc825be39c71a583351783e251
bluehomewu <bluehome.wu@gmail.com>
2021-03-10 15:57:47 +0800

    E2M: Change EVW to E2M

--- omni_E2M.mk
+++ omni_E2M.mk
@@ -21,16 +21,16 @@ $(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
-# Inherit from EVW device
-$(call inherit-product, device/fih/EVW/device.mk)
+# Inherit from E2M device
+$(call inherit-product, device/fih/E2M/device.mk)
 
 # Inherit some common Omni stuff.
 $(call inherit-product, vendor/omni/config/common.mk)
 $(call inherit-product, vendor/omni/config/gsm.mk)
 
 # Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := EVW
-PRODUCT_NAME := omni_EVW
+PRODUCT_DEVICE := E2M
+PRODUCT_NAME := omni_E2M
 PRODUCT_BRAND := FIH
 PRODUCT_MODEL := E2M
 PRODUCT_MANUFACTURER := fih
